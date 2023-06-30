a82aaa6d09f00c3e7d20d60860a1742f7feebe03
Artem <praedythbr@gmail.com>
2023-06-30 16:23:19 +0300

    +

--- pages/index.js
+++ pages/index.js
@@ -191,7 +191,7 @@ export default function Home() {
           {success ? (
             <button
               onClick={() => {
-                if (userCallable) {
+                if (userCallable && !noMatch) {
                   window.location.href = "tel:" + companyPhone;
                 } else {
                   console.error(
